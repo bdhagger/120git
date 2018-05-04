d123501be827e4357f6d34f47cbd3abeef825365
bdhagger <bdhagger@ucsc.edu>
2018-05-04 10:46:21 -0700

    Add all files

---
+++ HelloKitty.java
@@ -0,0 +1,8 @@
+public class HelloKitty {
+
+    public static void main(String[] args) {
+        // Prints "Hello, Kitty" to the terminal window.
+        System.out.println("Hello, Kitty");
+    }
+     
+}
