130a7daa09a11c68cdef5a4056831e780ab680c3
bdhagger <bdhagger@ucsc.edu>
2018-05-07 14:05:10 -0700

    try to add all branches

--- HelloKitty.java
+++ HelloKitty.java
@@ -2,7 +2,8 @@ public class HelloKitty {
 
     public static void main(String[] args) {
         // Prints "Hello, Kitty" to the terminal window.
-        System.out.println("Hello, Kitty");
+        int a = 6;
+	System.out.println("Hello, Kitty");
     }
      
 }
